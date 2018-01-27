a96b38af2f07373e96db706cd4b18a8167d3fc70
Aderbal Farias <aderbalfarias@hotmail.com>
2018-01-27 22:52:56 +0000

    implementing functions

--- src/assets/js/site.js
+++ src/assets/js/site.js
@@ -1,69 +1,69 @@
-$(document).ready(function($) {
+$(document).ready(function ($) {
     var $document = $(document);
     var $window = $(window);
     var $body = $('body');
     var $htmlBody = $('html, body');
     var $showMessage = $('#showMessage');
-    var $scrollToTop = $('.scroll-to-top');    
+    var $scrollToTop = $('.scroll-to-top');
     var $navbar = $('.navbar');
     var $navbarCollapse = $('.navbar-collapse');
     var $pageScrollClicked = $('a[class*=page-scroll]');
-    var screenHeight = $window.height();    
+    var screenHeight = $window.height();
     var sHeight = 100;
     var navHeight = 62.800;
     var widthScreenLg = 992;
     var oneThousand = 1000;
 
     //set height to home-box
-	$(".home-box").height(screenHeight*0.8);
-	$(".message-box").css({'marginTop':screenHeight*0.3});
+    $(".home-box").height(screenHeight * 0.8);
+    $(".message-box").css({ 'marginTop': screenHeight * 0.3 });
 
-	$("#alert>.close").click(function () {
-	    $showMessage.removeClass("show-alert");
-	    $showMessage.addClass("display-none");
+    $("#alert>.close").click(function () {
+        $showMessage.removeClass("show-alert");
+        $showMessage.addClass("display-none");
     });
-    
+
     //scroll buttom
-    $window.on('scroll', function(){        
-        if ($document.scrollTop() > sHeight){
+    $window.on('scroll', function () {
+        if ($document.scrollTop() > sHeight) {
             $navbar.addClass('shrink');
             $navbar.removeClass('nav-lg-none');
             $scrollToTop.fadeIn();
         }
-        else{
+        else {
             $navbar.removeClass('shrink');
             $navbar.addClass('nav-lg-none');
             $scrollToTop.fadeOut();
         }
     });
 
-    $window.on('load', function(){
-        $body.scrollspy({    
+    $window.on('load', function () {
+        $body.scrollspy({
             target: '#navigation'
         });
     });
 
-    $window.on('resize', function() {
+    $window.on('resize', function () {
         $body.scrollspy('refresh');
     });
-    
-    $pageScrollClicked.on('click', function(e) {
+
+    $pageScrollClicked.on('click', function (e) {
         var target = $(this).attr('href');
         PageScroll(target);
         e.preventDefault();
-    });    
-    
-    function PageScroll(target){        
-        var plusNavHeight = $window.width() > widthScreenLg 
-            ? $navbar.hasClass("shrink") 
-                ? 0 
+    });
+
+    function PageScroll(target) {
+        var plusNavHeight = $window.width() > widthScreenLg
+            ? $navbar.hasClass("shrink")
+                ? 0
                 : navHeight
             : 0;
-            
+
         $htmlBody.stop().animate({
             scrollTop: $(target).offset().top + plusNavHeight
         }, oneThousand, "swing");
-        
+
         //Retract the navigation after clicking on a item of the menu.
         $navbarCollapse.collapse('hide');
     };
@@ -112,5 +112,22 @@ $(document).ready(function($) {
     function ShowMessageNoTimeOut(kind, title, detail) {
         var timeoutSeg = 600;
         ShowMessage(kind, title, detail, timeoutSeg);
-    } 
+    }
+ 
+    var phrases = [
+        "Full Stack Developer",
+        "Software Engineer",
+        "Senior .Net Developer",
+    ];
+
+    var objTs = new TextScramble(document.querySelector("#textProfession"));
+    var counter = 0;
+    var next = function () {
+        objTs.setText(phrases[counter]).then(function () {
+            setTimeout(next, 1800);
+        });
+        counter = (counter + 1) % phrases.length;
+    };
+    next();
 });
+
